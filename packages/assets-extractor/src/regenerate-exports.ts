edc93c3246f8506cb0c168e080345ed8e2a830b1
William Strainchamps <william.strainchamps@gmail.com>
2025-12-19 13:44:06 +0100

    removed script to generate exports and changed packages names

--- packages/assets-extractor/src/regenerate-exports.ts
+++
@@ -1,8 +0,0 @@
-import { generateExportFiles } from './index';
-
-const uiPackagePath = process.argv[2] || './packages/ui';
-
-console.log('Regenerating export files with Base64 images...');
-await generateExportFiles(uiPackagePath);
-
-console.log('Done!');
